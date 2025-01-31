5da56d1bcb1da66f7ad4ffe6da7c08d605926a19
JPCabral04 <jpqcabral04@gmail.com>
2025-01-31 14:17:12 -0300

    refactor poke-list and poke-search components; integrate search functionality and update poke-list styles

--- src/app/shared/poke-search/poke-search.component.ts
+++ src/app/shared/poke-search/poke-search.component.ts
@@ -1,12 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-poke-search',
   standalone: false,
-  
   templateUrl: './poke-search.component.html',
   styleUrl: './poke-search.component.scss'
 })
-export class PokeSearchComponent {
+export class PokeSearchComponent implements OnInit{
 
+  @Output() public emmitSearch: EventEmitter<string> = new EventEmitter();
+
+  constructor() {}
+
+  ngOnInit(): void {
+      
+  }
+
+  public search(value: string) {
+    this.emmitSearch.emit(value);
+    
+  }
 }
